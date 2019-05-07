d525a791af5f2fb5428c1173c50c78e63f05f00b
Caio Lutfi <caiolutfi@hotmail.com>
2019-05-07 13:17:37 -0300

    Fim da aula 0014

--- js/description-product.js
+++ js/description-product.js
@@ -3,5 +3,11 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleclick);
 
 function handleclick() {
-  console.log("Foi!");
+  /* if ($heart.classList.contains("-active")) {
+    $heart.classList.remove("-active");
+  } else {
+    $heart.classList.add("-active");
+  }*/
+
+  $heart.classList.toggle("-active");
 }
